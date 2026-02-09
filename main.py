d2cd87ff5fdbd99f39fc90fc86a8ef8c4e829767
chrroth <roth.christoph@gmx.de>
2026-02-09 22:30:23 +0200

    added Player movement and rotation

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -23,15 +28,16 @@ def main():
 
         pygame.Surface.fill(screen, (0, 0, 0))
 
-        player.draw(screen)
+        updatable.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
-        
-
-
+    
 if __name__ == "__main__":
     main()
 
